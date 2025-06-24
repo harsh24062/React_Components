9f124d7dd1de96fe87e4fd4cd7bce30a76905e1d
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-06-24 11:53:29 +0530

    Added Otp boxes and added style

--- OTP_Input/src/App.jsx
+++ OTP_Input/src/App.jsx
@@ -1,13 +1,19 @@
-
+import { useState } from "react"
+import "./App.css"
 
 const OTP_DIGITS_COUNT = 5
 
 function App() {
 
+  const [inputArray,setInputArray] = useState(new Array(OTP_DIGITS_COUNT).fill(1))
+
   return (
-    <>  
-     
-    </>
+    <div className="App">  
+      <h1>Enter Your OTP Here</h1>  
+      {inputArray.map((input,index)=>{
+        return <input key={index} type="text" className="otp-input"/>
+      })}
+    </div>
   )
 }
 
