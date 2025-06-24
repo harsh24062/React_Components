c4498590eb0600789770691c4a748770e3c820fc
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-06-24 16:15:42 +0530

    First box gets focus, focus moves to adjacent box after entering value and Backspace feature added

--- OTP_Input/src/App.jsx
+++ OTP_Input/src/App.jsx
@@ -1,17 +1,45 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import "./App.css"
 
 const OTP_DIGITS_COUNT = 5
 
 function App() {
 
-  const [inputArray,setInputArray] = useState(new Array(OTP_DIGITS_COUNT).fill(1))
+  const [inputArray,setInputArray] = useState(new Array(OTP_DIGITS_COUNT).fill(""))
+  const refArr = useRef([])
+
+  const handleOnChange = (value,index) => {
+   console.log(value)
+   if(isNaN(value))return
+   const newArr=[...inputArray]
+   newArr[index]=value.slice(-1)
+   setInputArray(newArr)
+
+   if(value && index<OTP_DIGITS_COUNT-1){
+   refArr.current[index+1]?.focus()
+   }
+  }
+
+  const handleOnKeyDown = (key,index) => {
+    if(key=="Backspace" && inputArray[index]==="" && index>0){
+      refArr.current[index-1]?.focus()
+    }
+  }
+
+  useEffect(()=>{
+    refArr.current[0]?.focus()
+  },[])
 
   return (
     <div className="App">  
       <h1>Enter Your OTP Here</h1>  
       {inputArray.map((input,index)=>{
-        return <input key={index} type="text" className="otp-input"/>
+        return <input 
+        key={index} type="text"
+        className="otp-input" value={inputArray[index]}
+        ref={(ref)=>refArr.current[index]=ref}
+        onChange={(e)=>handleOnChange(e.target.value,index)}
+        onKeyDown={(e)=>handleOnKeyDown(e.key,index)}/>
       })}
     </div>
   )
