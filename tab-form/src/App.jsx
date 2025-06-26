2d91152059e46897f347d0c6aa4967b27d306388
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-06-26 20:08:57 +0530

    Making Tab-form (1). Added front page.

---
+++ tab-form/src/App.jsx
@@ -0,0 +1,13 @@
+import TabForm from "./components/TabForm"
+import "./App.css"
+
+function App() {
+  
+  return (
+    <div className="App">
+      <TabForm />
+    </div>
+  )
+}
+
+export default App
---
+++ tab-form/src/components/TabForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import Interests from "./Interests"
+import Profile from "./Profile"
+import Settings from "./Settings"
+
+const TabForm = () => {
+
+  const [activeTab,setActiveTab] = useState(0)
+
+  const tabs  = [
+  {
+    name:"Profile",
+    component:Profile
+  },
+  {
+    name:"Interests",
+    component:Interests
+  },
+  {
+    name:"Settings",
+    component:Settings
+  },
+]
+
+  const ActiveTabComponent = tabs[activeTab].component
+
+    return <div >
+        <div className="heading-container">
+        {tabs.map((tab,index)=> <div 
+        onClick={()=>{setActiveTab(index)}}
+        className="heading" key={index}>{tab.name}</div>)}  
+        </div>
+        <div className="tab-body">
+          <ActiveTabComponent />
+        </div>
+    </div>
+}
+
+export default TabForm
