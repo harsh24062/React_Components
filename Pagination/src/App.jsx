2026a83e9e5992bc55a07db204511d695722afe0
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-06-28 13:23:01 +0530

    Fetching done, made ProductCard. Pagination(2)

--- Pagination/src/App.jsx
+++ Pagination/src/App.jsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from "react"
+import "./App.css"
+import ProductCard from "./Components/ProductCard"
 
 function App() {
 
-  return (
-    <div>
-      
+  const [products,setProducts] = useState([])
+
+  useEffect(()=>{
+   fetch("https://dummyjson.com/products?limit=500").then(async(res)=>{
+    const json = await res.json()
+    setProducts(json.products)
+   })
+  },[])
+
+  return products.length==0?<div className="App"><h1>NO PRODUCT FOUND</h1></div>:(
+    <div className="App">
+      <h1>Paginatiom</h1>
+      {products.map((product,index)=>(<ProductCard key={index} title={product.title} description={product.description} price={product.price} thumbnail={product.thumbnail}/>))}
     </div>
   )
 }
---
+++ Pagination/src/Components/ProductCard.jsx
@@ -0,0 +1,11 @@
+
+const ProductCard = ({title,description,price,thumbnail})=>{
+  return <div className="product-card">
+      <h1>{title}</h1>
+      <img src={thumbnail} alt="Product Image" />
+      <h3>{description}</h3>
+      <h1>${price}</h1>
+  </div>
+}
+
+export default ProductCard
